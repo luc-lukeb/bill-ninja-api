1ef7f0eb44bc168ae286472e5cec6e54222dc84f
Maksim Izmaylov <me@kvakes.com>
2012-09-22 19:36:58 -0700

    DB migration for Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120923023200) do
+ActiveRecord::Schema.define(:version => 20120923023629) do
 
   create_table "bills", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
