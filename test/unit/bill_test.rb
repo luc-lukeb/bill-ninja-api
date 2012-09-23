d5d59463828fe727219d8ec338dbd78cf0de420a
Maksim Izmaylov <me@kvakes.com>
2012-09-23 16:31:39 -0700

    Test for Bill model

--- test/unit/bill_test.rb
+++ test/unit/bill_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class BillTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save Bill without title or owner or items" do
+    bill = Bill.new(:title => "Fake Bill Title")
+    assert !bill.save
+    bill = Bill.new(:owner => users(:one))
+    assert !bill.save
+    bill = Bill.new(:items => [items(:one)])
+    assert !bill.save
+  end
+
+  test "should save Bill with title and owner and items" do
+    bill = Bill.new(:title => "Fake Bill Title", :owner => users(:two), :items => [items(:two)])
+    assert bill.save
+  end
+
 end
