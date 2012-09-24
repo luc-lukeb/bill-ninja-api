e718df4ea9e236b0e5e84df174da56272f11af54
Maksim Izmaylov <me@kvakes.com>
2012-09-23 18:42:12 -0700

    User model migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120924012726) do
+ActiveRecord::Schema.define(:version => 20120924013937) do
 
   create_table "bills", :force => true do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20120924012726) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "password"
   end
 
 end
